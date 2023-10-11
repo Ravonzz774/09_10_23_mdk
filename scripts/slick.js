19603c653b750eae39429c4734e5c0f7cad2a5a5
Ravonzz774 <vovash174@gmail.com>
2023-10-11 15:41:43 +0500

    adaptive for mobile

--- scripts/slick.js
+++ scripts/slick.js
@@ -1,14 +1,33 @@
-$('.slider').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    centerMode: true,
-    centerPadding: "60px",
-    variableWidth: true,
-    dots: true,
-    infinite: true,
-    speed: 1000,
-    appendDots: $('.slider-dots'),
-    nextArrow: document.querySelector('.slider-right-arrow'),
-    prevArrow: document.querySelector('.slider-left-arrow ')
-});
+var slider = $('.slider');
+
+
+function initSlider() {
+    $('.slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        centerMode: true,
+        centerPadding: "60px",
+        variableWidth: true,
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        appendDots: $('.slider-dots'),
+        nextArrow: document.querySelector('.slider-right-arrow'),
+        prevArrow: document.querySelector('.slider-left-arrow ')
+    });
+}
+
+function destroySlider() {
+    if (slider.hasClass('slick-initialized')) {
+        slider.slick('unslick');
+    }
+}
+
+$(window).on('load resize', function() {
+    if ($(window).width() < 576) {
+      destroySlider();
+    } else {
+      initSlider();
+    }
+  });
